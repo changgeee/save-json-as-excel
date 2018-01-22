b86ed9ed0ceab054b0aed24dadf7489a619db6d0
liuyu <592889511@qq.com>
2018-01-22 17:51:50 +0800

    使用方式优化

--- src/jsonToExcel.js
+++ src/jsonToExcel.js
@@ -2,61 +2,74 @@
  * @Author: changge
  * @Date: 2017-08-15 11:07:05
  * @Last Modified by: changge
- * @Last Modified time: 2017-08-26 20:49:40
+ * @Last Modified time: 2018-01-22 17:49:31
  */
 (function (global) {
   'use strict';
   var JSONTOEXCEL = (function () {
     var jsonToXLS = (function () {
       var xlsTemp = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><meta name=ProgId content=Excel.Sheet> <meta name=Generator content="Microsoft Excel 11"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>${table}</table></body></html>'
-      return function (data, header) {
-        var xlsData = '', keys = []
-        if (header) {
-          xlsData += '<thead>'
-          for (var key in header) {
-            keys.push(key)
-            xlsData += '<th>' + header[key] + '</th>'
+      return function (dataSource, columns) {
+        var xlsData = '',
+          values = []
+        xlsData += '<thead>'
+        columns.forEach(function (item) {
+          xlsData += '<th>' + item.title + '</th>'
+          if (item.computed && typeof item.computed === 'function') {
+            values.push(item.computed)
+          } else if (item.dataIndex && typeof item.dataIndex === 'string') {
+            values.push(item.dataIndex)
+          } else {
+            values.push(null)
           }
-          xlsData += '</thead>'
-          xlsData += '<tbody>'
-          data.map(function (item, index) {
-            xlsData += '<tr>'
-            for (var i = 0; i < keys.length; i++) {
-              xlsData += '<td>' + item[keys[i]] + '</td>'
-            }
-            xlsData += '</tr>'
-          })
-          xlsData += '</tbody>'
-          return xlsTemp.replace('${table}', xlsData)
-        }
-        data.map(function (item, index) {
-          xlsData += '<tbody><tr>'
-          for (var key in item) {
-            xlsData += '<td>' + item[key] + '</td>'
-          }
-          xlsData += '</tr></tbody>'
         })
+        xlsData += '</thead>'
+        xlsData += '<tbody>'
+        dataSource.forEach(function (item) {
+          xlsData += '<tr>'
+          values.forEach(function (key) {
+            xlsData += '<td>'
+            if (typeof key === 'string') {
+              xlsData += item[key]
+            } else if (typeof key === 'function') {
+              xlsData += key(item) || ''
+            } else {
+              xlsData += ''
+            }
+            xlsData += '</td>'
+          })
+          xlsData += '</tr>'
+        })
+        xlsData += '</tbody>'
         return xlsTemp.replace('${table}', xlsData)
       }
     })()
 
 
-    var jsonToCSV = function (data, keys) {
-      var csvData = ''
-      if (keys) {
-        data.map(function (item) {
-          for (var i = 0; i < keys.length; i++) {
-            csvData += item[keys[i]] + ','
-          }
-          csvData = csvData.slice(0, csvData.length - 1)
-          csvData += '\r\n'
-        })
-        return csvData
-      }
-      data.map(function (item) {
-        for (var k in item) {
-          csvData += item[k] + ','
+    var jsonToCSV = function (dataSource, columns) {
+      var csvData = '',
+        values = []
+      columns.forEach(function (item) {
+        if (item.computed && typeof item.computed === 'function') {
+          values.push(item.computed)
+        } else if (item.dataIndex && typeof item.dataIndex === 'string') {
+          values.push(item.dataIndex)
+        } else {
+          values.push(null)
         }
+      })
+
+      dataSource.forEach(function (item) {
+        values.forEach(function (key) {
+          if (typeof key === 'string') {
+            csvData += item[key]
+          } else if (typeof key === 'function') {
+            csvData += key(item) || ''
+          } else {
+            csvData += ''
+          }
+          csvData += ','
+        })
         csvData = csvData.slice(0, csvData.length - 1)
         csvData += '\r\n'
       })
@@ -65,12 +78,12 @@
     var base64 = function (s) {
       return window.btoa(window.unescape(encodeURIComponent(s)))
     }
-    var exportXLS = function (data, fileName, header) {
-      var XLSData = 'data:application/vnd.ms-excel;base64,' + base64(jsonToXLS(data, header))
+    var exportXLS = function (dataSource, columns, fileName) {
+      var XLSData = 'data:application/vnd.ms-excel;base64,' + base64(jsonToXLS(dataSource, columns))
       download(XLSData, fileName)
     }
-    var exportCSV = function (data, fileName, keys) {
-      var CSVData = 'data:application/csv;base64,' + base64(jsonToCSV(data, keys))
+    var exportCSV = function (dataSource, columns, fileName) {
+      var CSVData = 'data:application/csv;base64,' + base64(jsonToCSV(dataSource, columns))
       download(CSVData, fileName)
     }
 
@@ -97,12 +110,17 @@
       }
     }
     var base64ToBlob = function (base64Data) {
-      var arr = base64Data.split(','), mime = arr[0].match(/:(.*?);/)[1],
-        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n)
+      var arr = base64Data.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n)
       while (n--) {
         u8arr[n] = bstr.charCodeAt(n)
       }
-      return new Blob([u8arr], { type: mime })
+      return new Blob([u8arr], {
+        type: mime
+      })
     }
     return {
       exportXLS: exportXLS,
@@ -110,7 +128,9 @@
     }
   }())
   if (typeof define === 'function' && define.amd) {
-    define(function () { return JSONTOEXCEL })
+    define(function () {
+      return JSONTOEXCEL
+    })
   } else if (typeof exports !== 'undefined') {
     if (typeof module !== 'undefined' && module.exports) {
       exports = module.exports = JSONTOEXCEL
